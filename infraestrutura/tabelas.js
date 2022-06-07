d519bf830022c50922be03d4feb4ff6632f24351
Lucas Fernandes <lucasfernandes.rf@gmail.com>
2022-06-07 12:08:09 -0300

    master

---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,32 @@
+class Tabelas {
+    init(conexao) {
+        this.conexao = conexao
+
+        this.criarAtendimento()
+        this.criarServico()
+    }
+
+    criarAtendimento(){
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        this.conexao.query(sql, erro => {
+            if(erro){
+                console.log(erro)
+            }else{
+                console.log('Tabela Atendimentos criada com sucesso')
+            }
+        })
+    }
+
+    criarServico(){
+        const sql = 'CREATE TABLE IF NOT EXISTS Servicos (id int NOT NULL AUTO_INCREMENT, nome varchar(50) NOT NULL, preco varchar(20) NOT NULL, PRIMARY KEY(id))'
+        this.conexao.query(sql, erro => {
+            if(erro){
+                console.log(erro)
+            }else{
+                console.log('Tabela Servicos criada com sucesso')
+            }
+        })
+    }
+}
+
+module.exports = new Tabelas;
